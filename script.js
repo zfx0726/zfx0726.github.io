5919c70e7ba85a3bba4f00f45db10b3e3ae8a29a
Forrest Xiao <zfx0726@gmail.com>
2023-09-28 13:50:29 -0500

    fix map colors and add labels

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const height = 500;
 
 // Create a color scale
 const colorScale = d3.scaleSequential()
-    .domain([0, 10000]) // Domain will be updated based on the negotiated rates in the CSV data
     .interpolator(d3.interpolateBlues);
 
 // Set up the map projection
@@ -32,28 +31,6 @@ const tooltip = d3.select('body').append('div')
 
 // Load and render the map
 d3.json(geojsonUrl).then(stateData => {
-    // Process the GeoJSON data here
-    svgMap.selectAll('path')
-        .data(stateData.features)
-        .enter()
-        .append('path')
-        .attr('d', path)
-        .attr('fill', d => colorScale(0)) // Initially, fill with the lowest color in the scale
-        .attr('stroke', 'white')
-        .on('mouseover', d => {
-            tooltip.transition()
-                .duration(200)
-                .style('opacity', .9);
-            tooltip.html(d.properties.NAME)
-                .style('left', (d3.event.pageX) + 'px')
-                .style('top', (d3.event.pageY - 28) + 'px');
-        })
-        .on('mouseout', d => {
-            tooltip.transition()
-                .duration(500)
-                .style('opacity', 0);
-        });
-
     // Load and render the bar chart
     d3.csv(csvUrl).then(csvData => {
         // Calculate average negotiated rates per state
@@ -64,17 +41,48 @@ d3.json(geojsonUrl).then(stateData => {
             stateRates[state].push(+d.negotiated_rate);
         });
 
-        // Calculate the average rate and update the color of the map
-        Object.keys(stateRates).forEach(state => {
-            const avgRate = d3.mean(stateRates[state]);
-            svgMap.selectAll('path')
-                .filter(d => d.properties.STUSPS === state)
-                .attr('fill', d => colorScale(avgRate));
-        });
+        // Calculate the minimum and maximum average rate
+        const avgRates = Object.values(stateRates).map(rates => d3.mean(rates));
+        const minRate = d3.min(avgRates);
+        const maxRate = d3.max(avgRates);
 
-        // Update the domain of the color scale and create the bar chart
-        const maxRate = d3.max(Object.values(stateRates).map(d => d3.mean(d)));
-        colorScale.domain([0, maxRate]);
+        // Update the domain of the color scale
+        colorScale.domain([minRate, maxRate]);
+
+        // Update the color of the map based on average negotiated rates
+        svgMap.selectAll('path')
+            .data(stateData.features)
+            .enter()
+            .append('path')
+            .attr('d', path)
+            .attr('fill', d => {
+                const state = d.properties.STUSPS;
+                const avgRate = stateRates[state] ? d3.mean(stateRates[state]) : minRate;
+                return colorScale(avgRate);
+            })
+            .attr('stroke', 'white')
+            .on('mouseover', d => {
+                tooltip.transition()
+                    .duration(200)
+                    .style('opacity', .9);
+                tooltip.html(d.properties.NAME)
+                    .style('left', (d3.event.pageX) + 'px')
+                    .style('top', (d3.event.pageY - 28) + 'px');
+            })
+            .on('mouseout', d => {
+                tooltip.transition()
+                    .duration(500)
+                    .style('opacity', 0);
+            });
+
+        // Add a title to the map visualization
+        svgMap.append('text')
+            .attr('x', width / 2)
+            .attr('y', 20)
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '20px')
+            .attr('font-weight', 'bold')
+            .text('Average Negotiated Rates by State');
 
         // Set up the SVG container for the bar chart
         const svgBar = d3.select('#bar-chart')
@@ -114,7 +122,7 @@ d3.json(geojsonUrl).then(stateData => {
                 tooltip.transition()
                     .duration(200)
                     .style('opacity', .9);
-                tooltip.html(d.state + '<br>' + 'Avg Rate: ' + d.avgRate.toFixed(2))
+                tooltip.html(d.state + '<br>' + d.avgRate.toFixed(2))
                     .style('left', (d3.event.pageX) + 'px')
                     .style('top', (d3.event.pageY - 28) + 'px');
             })
@@ -123,5 +131,14 @@ d3.json(geojsonUrl).then(stateData => {
                     .duration(500)
                     .style('opacity', 0);
             });
+
+        // Add title to the bar chart
+        svgBar.append('text')
+            .attr('x', width / 2)
+            .attr('y', 20)
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '20px')
+            .attr('font-weight', 'bold')
+            .text('Average Negotiated Rates by State');
     });
 });
