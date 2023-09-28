23050809f0bf671bd72a86cb1241c81961287217
Forrest Xiao <zfx0726@gmail.com>
2023-09-28 15:27:00 -0500

    add state mappings

--- script.js
+++ script.js
@@ -54,9 +54,12 @@ d3.json(geojsonUrl).then(stateData => {
             .append('path')
             .attr('d', path)
             .attr('fill', d => {
-                const state = d.properties.STUSPS; // Correctly access the state abbreviation from GeoJSON data
-                const avgRate = stateRates[state] ? d3.mean(stateRates[state]) : minRate;
-                return colorScale(avgRate);
+                const stateNumberMapping = {"01": "AL", "02": "AK", "04": "AZ", "05": "AR", "06": "CA", "08": "CO", "09": "CT", "10": "DE", "11": "DC", "12": "FL", "13": "GA", "15": "HI", "16": "ID", "17": "IL", "18": "IN", "19": "IA", "20": "KS", "21": "KY", "22": "LA", "23": "ME", "24": "MD", "25": "MA", "26": "MI", "27": "MN", "28": "MS", "29": "MO", "30": "MT", "31": "NE", "32": "NV", "33": "NH", "34": "NJ", "35": "NM", "36": "NY", "37": "NC", "38": "ND", "39": "OH", "40": "OK", "41": "OR", "42": "PA", "44": "RI", "45": "SC", "46": "SD", "47": "TN", "48": "TX", "49": "UT", "50": "VT", "51": "VA", "53": "WA", "54": "WV", "55": "WI", "56": "WY", "72": "PR"};
+    const stateNumber = d.properties.STATE;
+    const state = stateNumberMapping[stateNumber] || 'Unknown State'; // Correctly access the state abbreviation from GeoJSON data
+                    const avgRate = stateRates[state] || [];
+    const avgRateValue = avgRate.length > 0 ? d3.mean(avgRate) : minRate;
+                    return colorScale(avgRateValue);
             })
             .attr('stroke', 'white')
             .on('mouseover', d => {
@@ -117,7 +120,7 @@ d3.json(geojsonUrl).then(stateData => {
                 tooltip.transition()
                     .duration(200)
                     .style('opacity', .9);
-                tooltip.html(d.state + '<br>' + avgRateStr)
+                    tooltip.html(stateNumberMapping[d.state] + ' (' + d.state + ')' + '<br>' + avgRateStr)
                     .style('left', (d3.event.pageX) + 'px')
                     .style('top', (d3.event.pageY - 28) + 'px');
             })
